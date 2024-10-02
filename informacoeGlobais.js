02f9e44c5fbfa4da67071bc6148d30c27a5226d3
Lucas Miguel <106555557+LucasMiguel0@users.noreply.github.com>
2024-10-02 11:07:46 -0300

    02/10/2024

--- informacoeGlobais.js
+++ informacoeGlobais.js
@@ -7,14 +7,19 @@ todos os dados sejam enviadoss*/
 async function visualizarInformacoesGlobais(params) {
     //contante que armazena uma repossta  await=espera ////fetch=faz a requisição
     const res = await fetch(url);
-    const dados = await res.json();//espera as reposta erem convertidas em JSON
+    const dados = await res.json();//espera as reposta serem convertidas em JSON
+    const pessoasConectadas = (dados.total_pessoas_conectadas/1e9)//Cria a variável pessoasConectadas, sendo o total_pessoas_conectadas por um bilhão
+    const pessoasNoMundo=(dados.total_pessoas_mundo/1e9)//Cria a variável pessoasNoMundo, sendo o total_pessoas_mundo por um bilhao
+    const horas = parseInt(dados.tempo_medio)//Cria uma variável que utiliza apenas a parte inteira do número
+    const minutos = Math.round*(dados.tempo_medio - horas) * 60);//Cria uma variável que utiliza só a parte decimal das horas, transforma em minutos e arredonda
+
 console.log(dados);//visualizar as informações no console
 const paragrafo=document.createElement('p');//criar um elemento de parágrafo
 
 paragrafo.classList.add('graficos-container__texto');//adiciona uma classe do CSS ao parágrafo
 
-paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span>
-estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`//Insere o texto "Você sabia que o mundo tem"+total_pessoas_mundo
+paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.pessoasNoMundo} bilhões </span> de pessoas e que aproximadamente <span>${dados.pessoasConectadas} bilhões</span>
+estão conectadas em alguma rede social e passam em média <span>${dados.horas} horas e ${minutos} minutos</span> horas conectadas.`//Insere o texto "Você sabia que o mundo tem"+total_pessoas_mundo
  
 const container=document.getElementById('graficos-container');//cria a variável "container", seleciona o ID(graficos-container) na section do HTML
 container.appendChild(paragrafo);//Insere o parágrafo dentro do "container"
